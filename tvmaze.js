c2c5d5b64470d5d44668c3e719b4a89c476dcbe6
Nicholas <nick.orsi1992@gmail.com>
2023-11-15 15:58:42 -0800

    Added functionality to displayShows function according to the instructions.

--- tvmaze.js
+++ tvmaze.js
@@ -32,7 +32,11 @@ async function getShowsByTerm(searchInput) {
     filteredShow.id = show.show.id;
     filteredShow.name = show.show.name;
     filteredShow.summary = show.show.summary;
-    filteredShow.image = show.show.image.medium;
+    if(show.show.image === null) {
+      filteredShow.image = "https://tinyurl.com/tv-missing";
+    } else {
+      filteredShow.image = show.show.image.medium;
+    }
     return filteredShow;
 
     // Can retain the the order key/value pairs are added.
@@ -45,25 +49,6 @@ async function getShowsByTerm(searchInput) {
   });
 
   return filteredShows;
-
-  // return [
-  //   {
-  //     id: 1767,
-  //     name: "The Bletchley Circle",
-  //     summary:
-  //       `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-  //          women with extraordinary skills that helped to end World War II.</p>
-  //        <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-  //          normal lives, modestly setting aside the part they played in
-  //          producing crucial intelligence, which helped the Allies to victory
-  //          and shortened the war. When Susan discovers a hidden code behind an
-  //          unsolved murder she is met by skepticism from the police. She
-  //          quickly realises she can only begin to crack the murders and bring
-  //          the culprit to justice with her former friends.</p>`,
-  //     image:
-  //         "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-  //   }
-  // ]
 }
 
 
@@ -80,8 +65,8 @@ function displayShows(shows) {
         <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-              alt="Bletchly Circle San Francisco"
+              src=${show.image}
+              alt=${show.name}
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
